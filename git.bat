51f3c19c8cfdfc9919e033f4afd10ffd4134e4af
akkradet <akkradet7@gmail.com>
2021-01-19 10:04:08 +0700

    Automatically updated - 19/01/2021 10:04:08

--- git.bat
+++ git.bat
@@ -2,6 +2,6 @@
 d:
 cd Github\iptvthai.my.to
 git.exe add . 
-git.exe commit -m "Automatically updated - %date:~7,2%/%date:~4,2%/%date:~10,4%"
+git.exe commit -m "Automatically updated - %date:~7,2%/%date:~4,2%/%date:~10,4% %time:~0,2%:%time:~3,2%:%time:~6,2%"
 git.exe push origin master
 pause
