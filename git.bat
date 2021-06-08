421018b28ad0563596487470f0110a80123afb8a
akkradet <akkradet7@gmail.com>
2021-06-08 15:36:13 +0700

    Under Construction - 08/06/2021 15:36:12

--- git.bat
+++ git.bat
@@ -2,6 +2,7 @@
 d:
 cd Github\iptvthai.my.to
 git.exe add . 
-git.exe commit -m "Automatically updated - %date:~7,2%/%date:~4,2%/%date:~10,4% %time:~0,2%:%time:~3,2%:%time:~6,2%"
+git.exe commit -m "Under Construction - %date:~7,2%/%date:~4,2%/%date:~10,4% %time:~0,2%:%time:~3,2%:%time:~6,2%"
+rem git.exe commit -m "Automatically updated - %date:~7,2%/%date:~4,2%/%date:~10,4% %time:~0,2%:%time:~3,2%:%time:~6,2%"
 git.exe push origin master
 pause
